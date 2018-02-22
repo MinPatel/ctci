672ea4373c6be140964576888c8b1b6acaa9de74
MinPatel <minalipatel@gmail.com>
2018-02-21 20:44:41 -0800

    modified linked list

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -33,29 +33,41 @@ class UnorderedList:
         #return self.head
 
     def search_node(self, data):
-        isNodeFound = False
+        is_node_found = False
         current = self.head
-        while not isNodeFound and current!= None:
+        while not is_node_found and current != None:
             if current.get_node() == data:
-                isNodeFound = True
+                is_node_found = True
             else:
                 current = current.get_next_node()
-        return isNodeFound
+        return is_node_found
 
-    # def delete(self, data):
-    #     isNodeFound = UnorderedList.search_node(data)
-    #     if isNodeFound:
-    #         Node.data = Node.set_next_node(data)
-    #     else:
-    #         print("Node Not Found")##
+    def delete(self, data):
+        previous = None
+        current = self.head
+        is_node_found = False
+        while not is_node_found and current != None:
+            if self.head.data == data:
+                self.head = current.get_next_node()
+                is_node_found = True
+            elif current.get_node() == data:
+                previous.set_next_node(current.get_next_node())
+                is_node_found = True
+            else:
+                previous = current
+                current = current.get_next_node()
 
-    # def size_of_list(self):
-    #     count = 0
-    #     for each_node in Node:
-    #         count += 1
-    #     return count
+    def size_of_list(self):
+        count = 0
+        current = self.head
+        while current != None:
+            count += 1
+            current = current.get_next_node()
+        return count
 
 # temp = Node(93)
+# temp = Node(36)
+# temp = Node(45)
 # print(temp.get_node())
 
 mylist = UnorderedList()
@@ -64,8 +76,13 @@ mylist.add_node(37)
 mylist.add_node(54)
 mylist.add_node(76)
 mylist.add_node(67)
+mylist.delete(67)
+mylist.add_node(57)
+mylist.add_node(66)
+mylist.add_node(90)
+print(mylist.size_of_list())
 
-result = mylist.search_node(54)
+
 
 
 
